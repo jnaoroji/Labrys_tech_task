ef6a8f3c0359a7f69f076765c609a9ef30edc331
jnaoroji <jennynaoroji@gmail.com>
2023-11-03 14:18:01 +1000

    tyriung to fetch API data and created a server folder

--- Labrys/src/components/card.js
+++ Labrys/src/components/card.js
@@ -1,19 +1,52 @@
 import { Card, CardBody } from "@chakra-ui/react";
 import { Box, Flex } from "@chakra-ui/react";
 import { Text } from "@chakra-ui/react";
-
 import { SkeletonCircle } from "@chakra-ui/react";
 import { Navigate } from "react-router-dom";
 import useCryptoStore from "../pages/cryptoStore"; // Import the Zustand store
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
+// https://coinmarketcap.com/api/documentation/v1/#operation/getV1CryptocurrencyListingsLatest
+
+const API_KEY= "8b98703c-d8ba-47ee-8375-c96f56bd9223";
+
 
 export default function Crypto({ cardData }) {
+// const navigate = useNavigate();
   const cryptoStore = useCryptoStore((s) => s.myTokens);
   const handleClick = () => {
     // // Save the cardData to myTokens
     // cryptoStore.addToMyTokens(cardData);
     // //then navigate to myTokens
-    Navigate('../pages/myTokens');
+    // navigate('../pages/myTokens');
   };
+  const [cryptoData, setCryptoData] = useState([]);
+  let response = null;
+useEffect(() => {
+    const fetchCryptocurrencyData = async () => {
+        let response = null;
+        try {
+            response = await axios.get(
+                'v1/cryptocurrency/listings/latest?limit=20',
+                {
+                    headers: {
+                        'X-CMC_PRO_API_KEY': API_KEY,
+                    },
+                }
+            );
+        } catch (ex) {
+            console.log(ex);
+        }
+        if (response) {
+            const json = response.data;
+            console.log(json);
+            setCryptoData(json);
+        }
+    }
+
+    fetchCryptocurrencyData();
+}, []);
 
   console.log(cryptoStore)
   return (
